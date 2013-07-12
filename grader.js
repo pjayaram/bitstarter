b8f854d5c722ee1bd1d26a3c7f4402663f160f96
Prashant Jayaraman <pjayaram@calpoly.edu>
2013-07-12 05:35:46 +0000

    updated grader, checks.

--- grader.js
+++ grader.js
@@ -22,16 +22,18 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://hidden-eyrie-2717.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +51,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +63,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var doOutJson = function(checkJson) {
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_path>', 'Path to URL')
+	.parse(process.argv);
+
+    if (program.file) {
+       var checkJson = checkHtmlFile(program.file, program.checks);
+       doOutJson(checkJson);
+    }
+    else {
+       rest.get(program.url).on('complete', function(result) {
+	  fs.writeFileSync("temp.html", result);
+	  var checkJson = checkHtmlFile("temp.html", program.checks);
+	  doOutJson(checkJson);
+       });
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
